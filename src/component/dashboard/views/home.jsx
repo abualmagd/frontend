eb81d59b7d8e495f71a11264b144c3fbaadea5e8
abualmagd <ismailabualmagd@google.com>
2022-07-20 12:19:42 +0300

    search query 2

--- src/component/dashboard/views/home.jsx
+++ src/component/dashboard/views/home.jsx
@@ -5,14 +5,20 @@ export default function Home(){
 
     return (
             <section className="dash-view-home">
-                <div className="search-query">
-                    <form className="search-query-form">
-                   <input type="text"  placeholder="search"/>
-                    <input type="text" placeholder="popular/recent " /> 
-                    <input type="submit"  value="search"/>
+                <div className="search-section">
+                    <form className="search-section-form">
+                   <input type="text"  placeholder="Keywords" required/>
+                    <select name="search-type" id="searchtype" selected="popular">
+                      <option value="popular">popular</option>
+                      <option value="recent">rercent</option>
+                    </select>
+                    <input type="submit" className="search-section-button" value="search twitter"/>
                     </form>
-          
                 </div>
+
+                <section className="serch-result-section">
+                  here your search here 
+                </section>
 
            </section>
            
