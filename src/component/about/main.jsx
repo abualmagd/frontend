75458e00c5be23bd1d9a7f5e6017cce80463c5ea
abualmagd <ismailabualmagd@google.com>
2022-08-16 18:15:07 +0300

    start about 2

--- src/component/about/main.jsx
+++ src/component/about/main.jsx
@@ -2,12 +2,24 @@ import React from "react";
 import styles from "./about.module.css";
 
 export default function Main() {
+    const styl={
+        width:"180px",
+        height:"60px",
+        marginTop:"20px"
+    }
     return (
         <div className={styles.main}>
+            <section className={styles.main_left}>
+                <p className={styles.content}>Continous flow of
+                <br/>
+                 content creation 
+                  </p>
+                <p className={styles.save}>save time and effort in 
+                 manage your twitter accounts </p>
+                <a className={styles.link_free} style={styl} href="/#">Get started on Flowfed</a>
+            </section>
             <section>
-                <p className={styles.content}>Constant flow of creative content </p>
-                <p className={styles.save}>save time and effort in manage your twitter accounts </p>
-                <a className={styles.link_free} href="/#">Get started on Flowfed</a>
+                <img   className={styles.main_img} src="../assets/creative1.png" alt="the power of automation " />
             </section>
         </div>
     );
