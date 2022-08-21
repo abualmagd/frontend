dee3a62e9b6e0309950247b81a4ae4fef1712d5f
abualmagd <ismailabualmagd@google.com>
2022-08-21 15:09:59 +0300

    about and logo

--- src/component/about/header.jsx
+++ src/component/about/header.jsx
@@ -1,16 +1,33 @@
 import React from "react";
 import styles from "./about.module.css";
-
+import AnimatedText from 'react-animated-text-content';
 
 export default function Header(){
 
     return (
         <div className={styles.header}>
             <div className={styles.logo}>
-             <div className={styles.logo_icon}>#</div>
-            <h3 className={styles.logo_name}>Flowfed</h3>            
+            <AnimatedText
+  type="chars" // animate words or chars
+  animation={{
+    x: '200px',
+    y: '-20px',
+    scale: 1.1,
+    ease: 'ease-in-out',
+  }}
+  animationType="diagonal" //float diagonal   lights  rifle
+  interval={0.06}
+  duration={0.8}
+  tag="p"
+  className={styles.logo_name}
+  includeWhiteSpaces
+  threshold={0.1}
+  rootMargin="20%"
+>
+Flowfed
+</AnimatedText>
+                       
             </div>
-
             <div className={styles.links}>
                 <a href="/#" className={styles.link}>Price</a>
                 <a href="/#" className={styles.link}>Learn</a>
--- src/component/about/main.jsx
+++ src/component/about/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./about.module.css";
 
+
 export default function Main() {
     const styl={
         width:"180px",
